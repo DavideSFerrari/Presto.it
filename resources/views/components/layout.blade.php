53c31ce49a7f0c26e0c884da64622e21edb1de2d
Samuele Palazzo <samuelepalazzo7@gmail.com>
2023-06-26 20:57:20 +0200

    Aggiunto Layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Presto.it</title>
+</head>
+<body>
+    <div class="min-vh-100">
+        {{$slot}}
+    </div>
+</body>
+</html>
