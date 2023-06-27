92fab5ed29b54556b14403102f4c2811397134e8
MetalTek <marius03andriesc@gmail.com>
2023-06-27 19:28:42 +0200

    Rimosso scritta Livewire

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css','resources/js/app.js'])
     <title>Presto.it</title>
-    @livewireStyles
 </head>
 <body>
 
