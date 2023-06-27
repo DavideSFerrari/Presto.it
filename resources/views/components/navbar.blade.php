b8064e53fe0ce552288732d5742cdfec0fded33f
davide ferrari <dferrari722@gmail.com>
2023-06-27 19:19:47 +0200

    Modifica navbar e pagina welcome

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -26,6 +26,19 @@
         <li class="nav-item">
           <a class="nav-link disabled">Disabled</a>
         </li>
+            @if (Route::has('login'))
+                <ul class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                    @auth
+                        <li><a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a></li>
+                    @else
+                      <li><a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a></li>
+
+                        @if (Route::has('register'))
+                        <li><a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a></li>
+                        @endif
+                    @endauth
+</ul>
+            @endif
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
