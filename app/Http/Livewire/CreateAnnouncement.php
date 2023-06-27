df9892a4154cdc784181d548e64a5ebc2e48593d
Samuele Palazzo <samuelepalazzo7@gmail.com>
2023-06-27 21:07:30 +0200

    Dovrei aver finito form create

--- app/Http/Livewire/CreateAnnouncement.php
+++ app/Http/Livewire/CreateAnnouncement.php
@@ -3,14 +3,36 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Announcement;
 
 class CreateAnnouncement extends Component
 {
     
+    public $title;
+    public $description;
+    public $price;
+    public $detail;
+    public $image;
 
+
+    public function store(){
+
+        Announcement::create([
+            'title'=>$this->title,
+            'description'=>$this->description,
+            'price'=>$this->price,
+            'detail'=>$this->detail,
+            'image'=>$this->image,
+            
+        ]);
+
+       
+    }
 
     public function render()
     {
         return view('livewire.create-announcement');
     }
 }
+
+//'title','price','description','detail','image','category_id'
--- resources/views/livewire/create-announcement.blade.php
+++ resources/views/livewire/create-announcement.blade.php
@@ -8,19 +8,22 @@
     </div>
     <div class="mb-3">
         <label for="description"> Descrizione</label>
-        <textarea wire:model="body" type="text" class="form-control"></textarea>
+        <textarea wire:model="description" type="text" class="form-control"></textarea>
     </div>
     <div class="mb-3">
         <label for="price"> Prezzo </label>
-        <input wire:model="title" type="number" class="form-control">
+        <input wire:model="price" type="number" class="form-control">
     </div>
     <div class="mb-3">
         <label for="detail"> Dettagli </label>
         <input wire:model="detail" type="text" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="image"> Image </label>
+        <input wire:model="image" type="file" class="form-control">
     </div>
     <button type="submit" class="btn btn-primary shadow px-4 py-2">Crea</button>
 
    </form>
 </div>
 
-//'title','price','description','detail','image','category_id'
