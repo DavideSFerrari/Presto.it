6c800bf569c9e60c9010ab2cfb9d9d9c3a5ef8de
Samuele Palazzo <samuelepalazzo7@gmail.com>
2023-06-27 19:15:54 +0200

    Installato e creato component livewire

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css','resources/js/app.js'])
     <title>Presto.it</title>
+    @livewireStyles
 </head>
 <body>
 
@@ -15,5 +16,7 @@
     </div>
 
     <x-footer/>
+
+    @livewireScripts
 </body>
 </html>
