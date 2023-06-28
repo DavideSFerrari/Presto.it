f351bd890b8f93f805974cd8a1ad51a0a9e24184
MetalTek <marius03andriesc@gmail.com>
2023-06-28 20:27:46 +0200

    CSS: animazione navbar

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -17,9 +17,8 @@
     <body class="antialiased">
 
 
-            
+    <x-main>          
 
-                <x-navbar></x-navbar>
 
 
                 @if (Route::has('login'))
@@ -32,5 +31,6 @@
                 <x-footer></x-footer>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    </x-main>   
     </body>
 </html>
