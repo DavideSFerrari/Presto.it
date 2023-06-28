2189e6702958cdd963ffc644fe157d8caf2c5178
Tommaso Catalano <catalanotommaso84b28@hotmail.it>
2023-06-28 21:05:15 +0200

    Inserimento annunci in homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -32,7 +32,7 @@
     <div class="row">
         @foreach ($announcements as $announcement)
         <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="http://picsum.photos/200" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">{{$announcement->title}}</h5>
             <p class="card-text">{{$announcement->price}} euro</p>
