82eb30c1142b8f6bd081fd324206bbb066e66693
MetalTek <marius03andriesc@gmail.com>
2023-06-28 18:35:04 +0200

    prova

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -52,7 +52,7 @@
                                     </li>
                                 </button>
                             </form>
-                            <p>Ciao, {{ Auth::user()->email }},</p>
+                            <p class="my-2">Ciao, {{ Auth::user()->email }},</p>
                         @else
                             <li class="nav-item"><a class="nav-link" href="{{ route('login') }}"
                                     class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
