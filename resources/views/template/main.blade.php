b6f18ca95167dacf4fe71fadd6ae3ca368012806
davide ferrari <dferrari722@gmail.com>
2023-06-29 15:03:33 +0200

    Pulizia cdn e risoluzione problema della voce categorie nav

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="preconnect" href="https://fonts.bunny.net"> -->
+        <!-- <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> -->
+        <style>
+        </style>
     @vite(['resources/css/app.css','resources/js/app.js'])
     <title>Presto.it</title>
     @livewireStyles
@@ -11,14 +16,14 @@
 <body>
     
     
-    <x-navbar/>
+    <x-navbar></x-navbar>
     
     <div class="min-vh-100">
         {{$slot}}
     </div>
 
-    <x-footer/>
 
+    <x-footer/>
     @livewireScripts
 </body>
 </html>
