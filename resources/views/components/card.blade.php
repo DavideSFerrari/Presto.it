017e243d122dbc38bb717efbf6a8d5af1352c19e
Samuele Palazzo <samuelepalazzo7@gmail.com>
2023-06-29 20:55:00 +0200

    mando detail

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -6,13 +6,7 @@
               <p class="card-text">{{$announcement->price}} euro</p>
               <p class="card-text">{{$announcement->description}}</p>
               <p class="card-text">{{$announcement->detail}}</p>
-<<<<<<< HEAD
-              <a href="#" class="btn btn-primary m-1">Visualizza dettaglio</a>
-              <p class="card-footer">Pubblicato il: {{$announcement->created_at->format('d/m/Y')}}</p>
-              <p class="card-footer">Pubblicato da: </p>
-=======
               <a href="{{route('announcements.detail', $announcement)}}" class="btn btn-primary">Visualizza dettaglio</a>
->>>>>>> e74090bcd7450d10b5ca08b56800281d559edb23
               </div>
           </div>
           @endforeach
