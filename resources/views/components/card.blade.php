d6fe85c41eb8a30c0eb52a18400cc2fdcfe4ca98
MetalTek <marius03andriesc@gmail.com>
2023-06-29 21:48:01 +0200

    Css e blade di card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,14 +1,14 @@
 <div class="container">
     <div class="row"> 
         @foreach ($announcements as $announcement)
-          <div class="card m-3" style="width: 18rem;">
-              <img src="http://picsum.photos/200" class="card-img-top my-3" alt="...">
-              <div class="card-body">
-              <h5 class="card-title">{{$announcement->title}}</h5>
-              <p class="card-text">{{$announcement->price}} euro</p>
-              <p class="card-text">{{$announcement->description}}</p>
-              <p class="card-text">{{$announcement->detail}}</p>
-              <a href="{{route('announcements.detail', $announcement)}}" class="btn btn-primary">Visualizza dettaglio</a>
+          <div class=" m-3 product-card rounded-3" style="width: 18rem;">
+              <img src="http://picsum.photos/200" class="card-img-top my-3 rounded-3" alt="...">
+              <div class="product-details">
+                <p class="card-text product-catagory">{{$announcement->detail}}</p>
+              <h5 class="card-title"><a class="text-custom" href="{{route('announcements.detail', $announcement)}}">{{$announcement->title}}</a></h5>
+              <p class="py-2">{{$announcement->description}}</p>
+              <hr>
+              <p class="card-text product-price">{{$announcement->price}} euro</p>
               <p class="card-footer">Pubblicato il: {{$announcement->created_at->format('d/m/Y')}}</p>
               <p class="card-footer">Pubblicato da: </p>
               </div>
