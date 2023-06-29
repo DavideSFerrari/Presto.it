ec6bbbe7e0d78da801d8486206df1d04fd32e5b1
MetalTek <marius03andriesc@gmail.com>
2023-06-29 22:42:35 +0200

    Modifica css di card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,19 +1,22 @@
 <div class="container">
-    <div class="row"> 
+    <div class="row">
         @foreach ($announcements as $announcement)
-          <div class=" m-3 product-card rounded-3 card-custom shadow" style="width: 18rem;">
-              <img src="http://picsum.photos/200" class="card-img-top my-3 rounded-3" alt="...">
-              <div class="product-details">
-                <p class="card-text product-catagory">{{$announcement->detail}}</p>
-              <h5 class="card-title"><a class="text-custom" href="{{route('announcements.detail', $announcement)}}">{{$announcement->title}}</a></h5>
-              <p class="card-text py-2 text-break">{{$announcement->description}}</p>
-              <hr>
-              <p class="card-text product-price">{{$announcement->price}} euro</p>
-              <p class="card-footer">Pubblicato il: {{$announcement->created_at->format('d/m/Y')}}</p>
-              
-              </div>
-          </div>
-          @endforeach
+            <div class=" m-3 product-card rounded-3 card-custom shadow" style="width: 18rem;">
+
+                <a class="text-custom" href="{{ route('announcements.detail', $announcement) }}">
+                    <img src="http://picsum.photos/200" class="card-img-top my-3 rounded-3" alt="...">
+                    <div class="product-details">
+                        <p class="card-text product-catagory">{{ $announcement->detail }}</p>
+                        <h5 class="card-title">{{ $announcement->title }}</h5>
+                        <p class="card-text py-2 text-break text-lowercase">{{ $announcement->description }}</p>
+                        <hr class="text-dark">
+                        <p class="card-text product-price">{{ $announcement->price }} euro</p>
+                        <p class="card-footer">Pubblicato il: {{ $announcement->created_at->format('d/m/Y') }}</p>
+                </a>
+
+            </div>
     </div>
-    
+    @endforeach
+</div>
+
 </div>
