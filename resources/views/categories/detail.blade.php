f476046f5c53f590d3244c7b74014a087a81e0d9
MetalTek <marius03andriesc@gmail.com>
2023-06-30 20:08:23 +0200

    Modifica blade di detail

--- resources/views/categories/detail.blade.php
+++ resources/views/categories/detail.blade.php
@@ -1,22 +1,28 @@
 <x-main>
 
-<h1>Esplora la categoria {{$category->name}}</h1>
-<div class="container">
-    <div class="row">
-        @forelse ($category->announcements as $announcement)
-        <div class="card" style="width: 18rem;">
-            <img src="http://picsum.photos/200" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">{{$announcement->title}}</h5>
-            <p class="card-text">{{$announcement->price}} euro</p>
-            <p class="card-text">{{$announcement->description}}</p>
-            <p class="card-text">{{$announcement->detail}}</p>
-            <a href="#" class="btn btn-primary">Visualizza dettaglio</a>
-            </div>
-            <p>Autore: {{$announcement->user->name ?? ''}}</p>
+<h1 class="container text-center my-5">Esplora la categoria {{$category->name}}</h1>
+
+    <div class="container">
+        <div class="row">
+            @foreach ($category->announcements as $announcement)
+                <div class=" m-3 product-card rounded-3 card-custom shadow" style="width: 18rem;">
+    
+                    <a class="text-custom text-break" href="{{ route('announcements.detail', $announcement) }}">
+                        <img src="http://picsum.photos/200" class="card-img-top my-3 rounded-3" alt="...">
+                        <div class="product-details">
+                            <p class="card-text product-catagory text-break">{{ $announcement->detail }}</p>
+                            <h5 class="card-title text-break">{{ $announcement->title }}</h5>
+                            <p class="card-text py-2 text-break text-lowercase">{{ $announcement->description }}</p>
+                            <hr class="text-dark">
+                            <p class="card-text product-price">{{ $announcement->price }} euro</p>
+                            <p>Autore: {{$announcement->user->name ?? ''}}</p>
+                    </a>
+    
+                </div>
         </div>
         @endforeach
     </div>
-
+    
+    </div>
 
   </x-main> 
