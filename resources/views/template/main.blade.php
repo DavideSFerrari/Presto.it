4779cf332e179f80ae9c15a31fb5ea8c823776f7
davide ferrari <dferrari722@gmail.com>
2023-06-30 20:38:30 +0200

    Definizione colore background del main

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -7,6 +7,9 @@
         <!-- <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> -->
         <style>
+                body {
+                        background-image: linear-gradient(180deg, rgba(0%, 40%, 100%, 0.4), rgba(0%, 20%, 100%, 0.1));
+                    }
         </style>
     @vite(['resources/css/app.css','resources/js/app.js'])
     <title>Presto.it</title>
