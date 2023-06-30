deb12ddab72c738c8cd0da3524fedca7fa6f4ff8
MetalTek <marius03andriesc@gmail.com>
2023-06-30 18:41:01 +0200

    Modifica blade di card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -3,11 +3,11 @@
         @foreach ($announcements as $announcement)
             <div class=" m-3 product-card rounded-3 card-custom shadow" style="width: 18rem;">
 
-                <a class="text-custom" href="{{ route('announcements.detail', $announcement) }}">
+                <a class="text-custom text-break" href="{{ route('announcements.detail', $announcement) }}">
                     <img src="http://picsum.photos/200" class="card-img-top my-3 rounded-3" alt="...">
                     <div class="product-details">
-                        <p class="card-text product-catagory">{{ $announcement->detail }}</p>
-                        <h5 class="card-title">{{ $announcement->title }}</h5>
+                        <p class="card-text product-catagory text-break">{{ $announcement->detail }}</p>
+                        <h5 class="card-title text-break">{{ $announcement->title }}</h5>
                         <p class="card-text py-2 text-break text-lowercase">{{ $announcement->description }}</p>
                         <hr class="text-dark">
                         <p class="card-text product-price">{{ $announcement->price }} euro</p>
