5f127df6f7c70036c30a504b5b9c15f18e9e2534
Tommaso Catalano <catalanotommaso84b28@hotmail.it>
2023-07-03 20:26:35 +0200

    implementazione revisione annunci

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -25,7 +25,39 @@
                             <img src="http://picsum.photo/id/27/1200/400" class="img-fluid p-3 rounded" alt="...">
                         </div>
                     </div>
-        
+                    <button class="carousel-control-prev" type="button" data-bs-target="#showCarousel"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Precedente</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#showCarousel"
+                    data-bs-slide="next">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Successivo</span>
+                    </button>
+                </div>
+                <h5 class="card-title">Titolo: {{$announcement_to_check->title}}</h5>
+                <p class="card-text">Descrizione: {{$announcement_to_check->description}}</p>
+                <p class="card-footer">Pubblicato il: {{$announcement_to_check->created_at->format('d/m/Y')}}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <form action="{{route('revisor.accepted_announcement',['announcement'=>$announcement_to_check])}}" method="POST">
+                    @csrf
+                    @method('PATCH')
+                    <button type="submit" class="btn btn-success shadow">Accetta</button>
+                </form>
+            </div>
+            <div class="col-12 col-md-6 text end">
+                <form action="{{route('revisor.reject_announcement',['announcement'=>$announcement_to_check])}}" method="POST">
+                    @csrf
+                    @method('PATCH')
+                    <button type="submit" class="btn btn-danger shadow">Rifiuta</button>
+                </form>
+            </div>
+        </div>
+    </div>
     @endif
     
 </x-main>
