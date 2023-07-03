d1b3b2291a33c4cffbafaa47a09afb8c34d03b79
davide ferrari <dferrari722@gmail.com>
2023-07-03 21:41:10 +0200

    risoluzione temporanea problema accetta annunci

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -12,52 +12,36 @@
     @if ($announcement_to_check)
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <div id="showCarousel" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="corousel-item active">
-                            <img src="http://picsum.photo/id/27/1200/400" class="img-fluid p-3 rounded" alt="...">
-                        </div>
-                        <div class="corousel-item">
-                            <img src="http://picsum.photo/id/27/1200/400" class="img-fluid p-3 rounded" alt="...">
-                        </div>
-                        <div class="corousel-item">
-                            <img src="http://picsum.photo/id/27/1200/400" class="img-fluid p-3 rounded" alt="...">
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#showCarousel"
-                    data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Precedente</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#showCarousel"
-                    data-bs-slide="next">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Successivo</span>
-                    </button>
-                </div>
-                <h5 class="card-title">Titolo: {{$announcement_to_check->title}}</h5>
-                <p class="card-text">Descrizione: {{$announcement_to_check->description}}</p>
+
+        <div class=" m-3 product-card rounded-3 card-custom shadow" style="width: 18rem;">
+            <img src="https://picsum.photos/id/27/1200/400" class="card-img-top my-3 rounded-3" alt="...">
+            <div class="product-details">
+                <h5 class="card-title text-break">{{$announcement_to_check->title}}</h5>
+                <p class="card-text py-2 text-break text-lowercase">{{$announcement_to_check->description}}</p>
+                <hr class="text-dark">
                 <p class="card-footer">Pubblicato il: {{$announcement_to_check->created_at->format('d/m/Y')}}</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12 col-md-6">
+
+                <div>
+                <div class="col-6 col-md-3 text end">
                 <form action="{{route('revisor.accepted_announcement',['announcement'=>$announcement_to_check])}}" method="POST">
                     @csrf
                     @method('PATCH')
                     <button type="submit" class="btn btn-success shadow">Accetta</button>
                 </form>
-            </div>
-            <div class="col-12 col-md-6 text end">
-                <form action="{{route('revisor.reject_announcement',['announcement'=>$announcement_to_check])}}" method="POST">
-                    @csrf
-                    @method('PATCH')
-                    <button type="submit" class="btn btn-danger shadow">Rifiuta</button>
-                </form>
-            </div>
+                </div>
+            
+                <div class="col-6 col-md-3 text end">
+                    <form action="{{route('revisor.reject_announcement',['announcement'=>$announcement_to_check])}}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        <button type="submit" class="btn btn-danger shadow">Rifiuta</button>
+                    </form>
+                </div>
+                </div>
+
         </div>
     </div>
     @endif
     
 </x-main>
+
