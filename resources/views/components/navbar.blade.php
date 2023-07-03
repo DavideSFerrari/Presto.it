cf24b2be218edc3a8e5fb070c974c18a08950199
Tommaso Catalano <catalanotommaso84b28@hotmail.it>
2023-07-03 21:34:14 +0200

    implementazione accettazione annunci

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,69 +8,74 @@
 
         <div class="">
             <a class="navbar-brand anim" href="{{ route('homepage') }}">Presto.it</a>
-            </div>
+        </div>
 
-        <div class=""> 
+        <div class="">
 
-        
-                
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle anim" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Categorie
-                        </a>
-                        <ul class="dropdown-menu">
-                            @foreach ($categories as $category)
-                            <li><a class="dropdown-item" href="{{route('categories.detail', compact('category'))}}">{{$category->name}}</a></li>
-                            @endforeach
-                        </ul>   
-                    </li>     
-                    <li class="nav-item">
-                        <a class="nav-link anim" href="{{route('show')}}">Annunci</a>
-                    </li>
 
-                    <li class="nav-item">
-                        <x-_locale lang="en" nation='gb'/>
-                    </li>
-                    
-                    
-                    
-                    @if (Route::has('login'))
-                        @auth
+
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle anim" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Categorie
+                    </a>
+                    <ul class="dropdown-menu">
+                        @foreach ($categories as $category)
+                            <li><a class="dropdown-item"
+                                    href="{{ route('categories.detail', compact('category')) }}">{{ $category->name }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link anim" href="{{ route('show') }}">Annunci</a>
+                </li>
+
+                <li class="nav-item">
+                    <x-_locale lang="en" nation='gb' />
+                </li>
+
+
+
+                @if (Route::has('login'))
+                    @auth
                         <div class="sezioneUtente">
-                        <a class="nav-link anim mx-2" href="#" class="my-2">Ciao {{ Auth::user()->name }}, cosa vuoi fare?</a>
+                            <a class="nav-link anim mx-2" href="#" class="my-2">Ciao {{ Auth::user()->name }}, cosa
+                                vuoi fare?</a>
                         </div>
 
                         <li class="nav-item">
-                                @if (Route::has('login'))
+                            @if (Route::has('login'))
                                 @auth
-                                <a class="btn anim2 p-2" href="{{route('announcements.create')}}" role="button">Inserisci annuncio</a>
+                                    <a class="btn anim2 p-2" href="{{ route('announcements.create') }}" role="button">Inserisci
+                                        annuncio</a>
                                 @endauth
-                                @endif
-                            
-@if (Auth::user()->is_revisor)
-<li class="nav-item">
-    <a class="nav-link btn btn-outline-success btm-sm position-relative" aria-current="page" href="{{route('revisor.index')}}">Zona Revisore
-        <span class="position-absolulute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            {{App\Models\Announcement::toBeRevisionedCount()}}
-            <span class="visually-hidden">Unread Message</span>
-        </span>
-    </a>
-</li>
-@endif
-                            <form action="{{ route('logout') }}" method="POST">
-                                @csrf
-                                <button class="btn-1" onclick="event.preventDefault(); this.closest('form').submit();">
-                                    <li class="nav-item">
-                                        <a class="nav-link anim ">
-                                            Logout
-                                        </a>
-                                    </li>
-                                </button>
-                            </form>
-                
+                            @endif
+
+                            @if (Auth::user()->is_revisor)
+                        <li class="nav-item">
+                            <a class="nav-link btn btn-outline-success btm-sm position-relative" aria-current="page"
+                                href="{{ route('revisor.index') }}">Zona Revisore
+                                <span class="position-absolulute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                    {{ App\Models\Announcement::toBeRevisionedCount() }}
+                                    <span class="visually-hidden">Unread Message</span>
+                                </span>
+                            </a>
                         </li>
+                    @endif
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button class="btn-1" onclick="event.preventDefault(); this.closest('form').submit();">
+                            <li class="nav-item">
+                                <a class="nav-link anim ">
+                                    Logout
+                                </a>
+                            </li>
+                        </button>
+                    </form>
+
+                    </li>
                 @else
                     <li class="nav-item"><a class="nav-link anim" href="{{ route('login') }}"
                             class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 ">Log
@@ -83,10 +88,10 @@
                         </li>
                     @endif
                 @endauth
-            @endif
+                @endif
 
-        </ul>
-</div>
-    
-</div>
+            </ul>
+        </div>
+
+    </div>
 </nav>
