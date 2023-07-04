71a22ccb71992913259392639da76319b80115e9
Samuele Palazzo <samuelepalazzo7@gmail.com>
2023-07-04 18:54:57 +0200

    Inserito revisore

--- app/Http/Middleware/SetLocaleMiddleware.php
+++ app/Http/Middleware/SetLocaleMiddleware.php
@@ -4,7 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\App;
+
 
 class SetLocaleMiddleware
 {
@@ -13,10 +14,9 @@ class SetLocaleMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
-        $localeLanguage = session('locale','it');
-        App::setLocale($localeLanguage);
+        
         return $next($request);
     }
 }
