3d1b22011421b9a12d3a9331e311dbf0f097ce8f
MetalTek <marius03andriesc@gmail.com>
2023-07-04 21:08:52 +0200

    Revisor: Modifiche carusel

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -3,8 +3,8 @@
     <div class="container-fluid p-5 bg-gradient bg-success p-5 shadow mb-4">
         <div class="row">
             <div class="col-12 text-light p-5">
-                <h1 class="display-2">
-                    {{$announcement_to_check ? 'Ecco gli annunci da revisionare' : 'Non ci sono annunci da revisionare'}}
+                <h1 class="display-2 text-center">
+                    {{$announcement_to_check ? 'Annunci da revisionare' : 'Non ci sono annunci da revisionare'}}
                 </h1>
             </div>
         </div>
