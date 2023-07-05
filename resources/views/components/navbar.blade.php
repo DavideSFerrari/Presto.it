a4a688a4ecd7b4cea91fa026d519052602670033
davide ferrari <dferrari722@gmail.com>
2023-07-05 19:20:28 +0200

    Implementazione barra di ricerca annunci nella navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -90,6 +90,10 @@
                     @endauth
                 @endif
 
+                <form action="{{route('announcements.search')}}" method="GET" class="d-flex">
+	                <input name="searched" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+	                <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
             </ul>
         </div>
 
