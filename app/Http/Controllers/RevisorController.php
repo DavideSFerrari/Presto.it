8a686634254e779ea2e57703d786309e1f6e730f
davide ferrari <dferrari722@gmail.com>
2023-07-05 19:12:03 +0200

    Implementazione funzione searchAnnouncements in RevisorController

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -19,32 +19,34 @@ class RevisorController extends Controller
 
         $announcement_to_check= Announcement::where('is_accepted', null)->first();
         return view('revisor.index', compact('announcement_to_check'), compact('announcement'));
-        
-        
             }
 
     public function acceptAnnouncement(Announcement $announcement){
 
         $announcement->setAccepted(true);
         return redirect()->back()->with('message', 'Hai accettato l\'annuncio');
-}
-public function rejectAnnouncement(Announcement $announcement){
+            }
+
+    public function rejectAnnouncement(Announcement $announcement){
 
     $announcement->setAccepted(false);
     return redirect()->back()->with('message', 'Hai rifiutato l\'annuncio');
-}
+            }
 
-public function becomeRevisor(){
-   $obj = new BecomeRevisor(Auth::user());
+    public function becomeRevisor(){
+   
+    $obj = new BecomeRevisor(Auth::user());
     Mail::to('admin@presto.it')->send($obj);
-    
     return redirect()->back()->with('message','Complimenti! Richiesta effettuata correttamente');
-}
+            }
 
-public function makeRevisor(User $user){
+    public function makeRevisor(User $user){
     Artisan::call('presto:make_user_revisor', ["email"=>$user->email]);
     return redirect ('/')->with('message','Complimenti L\'utente è diventato revisore');
-}
+            }
 
-
+    public function searchAnnouncements(Request $request){
+    $announcements = Announcement::search($request->searched)->where('is_accepted', true)->paginate(10);
+    return view('show', compact('announcements'));
+            }
 }
