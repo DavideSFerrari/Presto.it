084bab5b8a68af56af62791f27863e7463c93106
davide ferrari <dferrari722@gmail.com>
2023-07-10 19:44:02 +0200

    aggiornamento

--- resources/views/announcements/detail.blade.php
+++ resources/views/announcements/detail.blade.php
@@ -1,48 +1,45 @@
 <x-main>
-    <!-- <div class="container text-center">
-        <div class="row">
-            <div class="col-12 text-dark p-5">
-                <h1 class="display-2">Dettaglio annuncio</h1>
-            </div>
-        </div>
-    </div> -->
-    <div class="container bg-black p-custom-2 announcement-detail">
-      <div class="d-flex">
-        <section class="w-50">
-            <div id="carouselExampleIndicators" class="carousel slide">
-                <div class="carousel-inner">
-                    <div class="carousel-item active ">
-                        <img src="{{ url('/img/prova1.jpg') }}" class="d-block w-100 " alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="{{ url('/img/prova2.jpg') }}" class="d-block  w-100 " alt="...">
-                    </div>
-                    <div class="carousel-item ">
-                        <img src="{{ url('/img/gatto3.jpg') }}" class="d-block  w-100 " alt="...">
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </div>
-        </section>
 
-            <section class="m-custom w-50 detail-container">
-            <p class="card-text mt-2 w-100">{{ $announcement->category->name }} - Pubblicato il: {{ $announcement->created_at->format('d/m/Y')}}</p>
-            <hr>
+    <div class="container w-75 bg-black p-custom-2 announcement-detail">
+        <div class="">
+
+            <section class="w-75 m-auto">
+                <div id="carouselExampleIndicators" class="carousel slide">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active ">
+                            <img src="{{ url('/img/prova1.jpg') }}" class="d-block w-100 " alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{ url('/img/prova2.jpg') }}" class="d-block  w-100 " alt="...">
+                        </div>
+                        <div class="carousel-item ">
+                            <img src="{{ url('/img/gatto3.jpg') }}" class="d-block  w-100 " alt="...">
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </section>
+
+            <section class="w-75 m-auto show-container">
+                <p class="card-text mt-2 w-100">{{ $announcement->category->name }} - Pubblicato il: {{ $announcement->created_at->format('d/m/Y')}}</p>
+                <hr>
                 <h3 class="card-title text-uppercase">{{ $announcement->title }}</h5>
                 <br>
                 <p class="card-text">Descrizione: {{ $announcement->description }}</p>
                 <p class="card-text">Dettagli: {{ $announcement->detail }}</p>
                 <p class="card-text">Prezzo: {{ $announcement->price }}€</p>
             </section>
+        
         </div>
     </div>
+
 </x-main>
--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -31,7 +31,7 @@
     
     <x-navbar></x-navbar>
     
-    <div class="min-vh-100 mt-5">
+    <div class="min-vh-100">
         {{$slot}}
     </div>
 
