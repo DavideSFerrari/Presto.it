bab76a1cdc0092adffadb8ffddf5a6f7320af29f
davide ferrari <dferrari722@gmail.com>
2023-07-11 21:40:16 +0200

    aggiornamento

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -13,16 +13,48 @@
                             
     @if ($announcement_to_check)
         <div class="container">
-            <div id="carouselExampleIndicators" class="carousel slide">
-                @if ($announcement_to_check->images)
-                <div class="carousel-inner">
-                    @foreach ($announcement_to_check->images as $image)
-                    <div class="carousel-item @if($loop->first)active @endif">
-                        <img src="{{Storage::url($image->path)}}" class="img-fluid p-3 rounded" alt="">
-                    </div>
-                    @endforeach
-                </div>
-                @endif
+
+                    <div id="carouselExampleIndicators" class="carousel slide">
+                    @if ($announcement_to_check->images->isNotEmpty())
+                        <div class="carousel-inner">
+                            @foreach ($announcement_to_check->images as $image)
+                            <div class="carousel-item @if($loop->first)active @endif">
+                                <img src="{{Storage::url($image->path)}}" class="img-fluid p-3 rounded" alt="">
+                            </div>
+                            @endforeach
+                        </div>
+                    @else
+                        <div class="carousel-inner">
+
+                            <div class="carousel-item active">
+                                <img src="https://picsum.photos/400/350" class="img-fluid p-3 rounded" alt="..." style="display: block; margin: auto;">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://picsum.photos/400/350" class="img-fluid p-3 rounded" alt="...">
+                            </div>
+                            <div class="carousel-item ">
+                                <img src="https://picsum.photos/400/350" class="img-fluid p-3 rounded" alt="...">
+                            </div>
+
+                        </div>
+                    @endif
+
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button> 
+
+                        </div>
+
+        </div>
+
+
 
 
                 <div class="revisor-container">
@@ -30,33 +62,28 @@
                     <p>{{ $announcement_to_check->description }}</p>
                     <p class="text-uppercase">{{ $announcement_to_check->category->name }}</p>
                     <p class="card-footer">Pubblicato il: {{ $announcement_to_check->created_at->format('d/m/Y') }}
-                        <div>
-                            <div class="d-flex ">
-                                <form
-                                    action="{{ route('revisor.accepted_announcement', ['announcement' => $announcement_to_check]) }}"
-                                    method="POST">
-                                    @csrf
-                                    @method('PATCH')
-                                    <button type="submit" class="btn btn-success shadow">Accetta</button>
-                                </form>
+                    <div>
+                        <div class="d-flex ">
+                            <form action="{{ route('revisor.accepted_announcement', ['announcement' => $announcement_to_check]) }}" method="POST">
+                                @csrf
+                                @method('PATCH')
+                                <button type="submit" class="btn btn-success shadow">Accetta</button>
+                            </form>
         
                                 <!-- <div class="col-6 col-md-3 text end"> -->
-                                <form
-                                    action="{{ route('revisor.reject_announcement', ['announcement' => $announcement_to_check]) }}"
-                                    method="POST">
-                                    @csrf
-                                    @method('PATCH')
-                                    <button type="submit" class="btn btn-danger shadow">Rifiuta</button>
-                                </form>
+                            <form action="{{ route('revisor.reject_announcement', ['announcement' => $announcement_to_check]) }}" method="POST">
+                                @csrf
+                                @method('PATCH')
+                                <button type="submit" class="btn btn-danger shadow">Rifiuta</button>
+                            </form>
                                                                
                                 <!-- </div> -->
-                            </div>
                         </div>
-                </div>
-                
+                    </div>
+                </div> 
 
-            </div>
-        </div>
+            
+
     @endif
 
 </x-main>
