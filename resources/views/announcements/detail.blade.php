fc71a5ab8904d8f78757d18d0e227aa9b95c96a1
Tommaso Catalano <catalanotommaso84b28@hotmail.it>
2023-07-11 19:13:48 +0200

    aggiunta immagini carosello

--- resources/views/announcements/detail.blade.php
+++ resources/views/announcements/detail.blade.php
@@ -5,7 +5,15 @@
 
             <section class="w-75 m-auto">
                 <div id="carouselExampleIndicators" class="carousel slide">
-                    
+                    @if ($announcement->images)
+                        <div class="carousel-inner">
+                            @foreach ($announcement->images as $image)
+                                <div class="carousel-item @if($loop->first)active @endif">
+                                <img src="{{Storage::url($image->path)}}" class="img-fluid p-3 rounded" alt="">
+                                </div>
+                             @endforeach
+                        </div>
+                @endif
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img src="{{ url('/img/prova1.jpg') }}" class="d-block w-100 " alt="...">
