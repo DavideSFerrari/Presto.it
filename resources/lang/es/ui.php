7abedf10a28111ba46511e3dc8fd2f6a5e275783
Samuele Palazzo <samuelepalazzo7@gmail.com>
2023-07-11 19:25:50 +0200

    Inserite altre traduzioni

--- resources/lang/es/ui.php
+++ resources/lang/es/ui.php
@@ -1,5 +1,6 @@
 <?php
 return [
     'allAnnouncements'=>'Eccos los nostros annuncios!',
-    'create'=>'Inserta tu anuncio'
+    'create'=>'Inserta tu anuncio!',
+    'home'=>'¡Aquí están los últimos anuncios!'
 ];
--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -9,7 +9,7 @@
             </div>
         </div>
     </div>
-
+    
                             
     @if ($announcement_to_check)
         <div class="container">
