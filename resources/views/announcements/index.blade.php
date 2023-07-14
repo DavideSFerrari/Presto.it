0d57132b6382baf2a5f34174e571efa6baa38c14
davide ferrari <dferrari722@gmail.com>
2023-07-14 21:19:42 +0200

    agg

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -2,11 +2,11 @@
 
     <div class="row mx-5">
 
-    <h3 class="text-center my-5 fs-1">{{__('ui.allAnnouncements')}}</h3> 
-        <x-card :$announcements>
+        <h3 class="text-center my-5 fs-1">{{__('ui.allAnnouncements')}}</h3> 
+            <x-card :$announcements>
 
-        </x-card>
-    </div>
+            </x-card>
+        </div>
     
 </x-main>   
 
