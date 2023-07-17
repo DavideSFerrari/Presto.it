a860b76836285f2cc8fea45280afe761780bfdf1
Samuele Palazzo <samuelepalazzo7@gmail.com>
2023-07-17 19:59:49 +0200

    continuo funzione handle

--- app/Jobs/RemoveFaces.php
+++ app/Jobs/RemoveFaces.php
@@ -14,19 +14,30 @@ class RemoveFaces implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     private $announcement_image_id;
-    /**
-     * Create a new job instance.
-     */
+    
     public function __construct($announcement_image_id)
     {
         $this->announcement_image_id = $announcement_image_id ;
     }
 
-    /**
-     * Execute the job.
-     */
+    
     public function handle(): void
     {
-        
+        $i = Image::find($this->announcement_image_id);
+        if (!$i){
+            return;
+        }
+        $srcPath = storage_path('app/public/' . $i->path);
+        $image = file_get_contents($srcPath);
+
+        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google_credential.json'));
+
+        $imageAnnotator = new ImageAnnotatorClient();
+        $response = $imageAnnotator->faceDetection($image);
+        $faces = $response->getFaceAnnotations();
+
+        @foreach ($faces as $face){
+            $vertices = $face->getBoundingPoly()->getVertices();
+        }
     }
 }
