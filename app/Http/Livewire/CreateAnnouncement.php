59065a9bbb8a592e06d58bb7d8b22be254535fde
Tommaso Catalano <catalanotommaso84b28@hotmail.it>
2023-07-17 20:36:16 +0200

    us07 completa

--- app/Http/Livewire/CreateAnnouncement.php
+++ app/Http/Livewire/CreateAnnouncement.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
 use App\Jobs\ResizeImage;
 use Livewire\Component;
@@ -79,6 +80,7 @@ public function removeImage($key)
             $newImage = $this->announcement->images()->create(['path'=>$image->store($newFileName,'public')]);
             dispatch(new ResizeImage($newImage->path , 300 , 400));
             dispatch(new GoogleVisionSafeSearch($newImage->id));
+            dispatch(new GoogleVisionLabelImage($newImage->id));
         }
 
         File::deleteDirectory(storage_path('/app/livewire-tmp'));
