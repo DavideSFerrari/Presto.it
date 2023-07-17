745315f386e8e50fbb5833831c38fc8673a9c80f
Samuele Palazzo <samuelepalazzo7@gmail.com>
2023-07-17 20:37:26 +0200

    problema function in remove faces

--- app/Jobs/RemoveFaces.php
+++ app/Jobs/RemoveFaces.php
@@ -1,19 +1,20 @@
 <?php
 
 namespace App\Jobs;
-
+use Spatie\Image\Image as SpatieImage;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 
 class RemoveFaces implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    private $announcement_image_id;
+   private $announcement_image_id;
     
     public function __construct($announcement_image_id)
     {
@@ -36,8 +37,29 @@ class RemoveFaces implements ShouldQueue
         $response = $imageAnnotator->faceDetection($image);
         $faces = $response->getFaceAnnotations();
 
-        @foreach ($faces as $face){
+        @foreach ($faces as $face) {
             $vertices = $face->getBoundingPoly()->getVertices();
+
+            $bounds = [];
+            @foreach ($vertices as vertex) {
+                $bounds[] = [$vertex->getX(), $vertex->getY()];
+            }
+
+            $w = $bounds[2][0] - $bounds[0][0];
+            $h = $bounds[2][1] - $bounds[0][1];
+
+            $image = SpatieImage::load($srcPath);
+
+            $image->watermark(base_path('resources/img/smile.png'))
+                ->watermarkPosition('top-left')
+                ->watermarkPadding($bounds[0][0], $bounds[0][1])
+                ->watermarkWidth($w, Manipulations::UNIT_PIXELS)
+                ->watermarkHeight($h, Manipulations::UNIT_PIXELS)
+                ->watermarkFit( Manipulations::FIT_STRETCH);
+
+                $image->save($srcPath);
         }
-    }
+
+        $imageAnnotator->close();
+    } 
 }
