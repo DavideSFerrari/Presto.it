f4f54c81289194a259c37fc1e464d483e0d4e9c1
Samuele Palazzo <samuelepalazzo7@gmail.com>
2023-07-19 21:29:12 +0200

    Problemi con edit

--- app/Http/Livewire/AnnouncementEditForm.php
+++ app/Http/Livewire/AnnouncementEditForm.php
@@ -28,39 +28,21 @@ class AnnouncementEditForm extends Component
         
     }
 
-    public function update()
-    {
+    public function update(){
         $this->validate();
         $this->announcement->update([
             'title' => $this->title,
+            'description' => $this->description,
             'price' => $this->price,
+            'detail' => $this->detail,
+            'images' => $this->images,
             'category' => $this->category,
-             'description' => $this->description,
         ]);
 
         
-    if (count($this->images)){
-        foreach ($this->images as $image){
-            
-            $newFileName = "announcements/{$this->announcement->id}";
-            $newImage = $this->announcement->images()->create(['path'=>$image->store($newFileName,'public')]);
-            
-            RemoveFaces::withChain([
-            new ResizeImage($newImage->path , 300 , 400),
-            new GoogleVisionSafeSearch($newImage->id),
-            new GoogleVisionLabelImage($newImage->id)
-            ])->dispatch($newImage->id);
-            
-        }
-
-        File::deleteDirectory(storage_path('/app/livewire-tmp'));
-}
-
-        $this->announcement->user()->associate(Auth::user());
-        $this->announcement->save();
+    }
 
     public function render()
-
     {
         return view('livewire.announcement-edit-form');
     }
