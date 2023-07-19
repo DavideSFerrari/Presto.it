6d668bf63e9eea8e9df5858ab96685c377806f24
MetalTek <marius03andriesc@gmail.com>
2023-07-19 20:43:19 +0200

    User_profile: modifiche front-end

--- resources/views/user_profile/index.blade.php
+++ resources/views/user_profile/index.blade.php
@@ -69,16 +69,7 @@
                               <hr>
                               <div class="row">
                                 <div class="col-sm-3">
-                                  <h6 class="mb-0">Phone</h6>
-                                </div>
-                                <div class="col-sm-9 text-secondary">
-        
-                                </div>
-                              </div>
-                              <hr>
-                              <div class="row">
-                                <div class="col-sm-3">
-                                  <h6 class="mb-0">Mobile</h6>
+                                  <h6 class="mb-0">Mobile/Phone</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
         
@@ -88,6 +79,15 @@
                               <div class="row">
                                 <div class="col-sm-3">
                                   <h6 class="mb-0">Address</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+        
+                                </div>
+                              </div>
+                              <hr>
+                              <div class="row">
+                                <div class="col-sm-3">
+                                  <h6 class="mb-0">Site</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
                                     
